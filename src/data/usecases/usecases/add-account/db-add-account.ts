057296aecf739d1af5ae1fdf244c390971c2e776
Richard Lopes <richard.cirux@gmail.com>
2020-03-25 23:35:50 -0300

    fix: add await on encrypt method

--- src/data/usecases/usecases/add-account/db-add-account.ts
+++ src/data/usecases/usecases/add-account/db-add-account.ts
@@ -6,14 +6,14 @@ import {
 import { Encrypter } from '../../../protocols/encrypter'
 
 export class DbAddAccount implements AddAccount {
-  private readonly encrypter;
+  private readonly encrypter: Encrypter;
 
   constructor (encrypter: Encrypter) {
     this.encrypter = encrypter
   }
 
   async add (account: AddAccountModel): Promise<AccountModel> {
-    this.encrypter.encrypt(account.password)
+    await this.encrypter.encrypt(account.password)
     return new Promise(resolve => resolve(null))
   }
 }
