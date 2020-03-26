21655cfa8a9543559779550261bbbbd9c2f8f5ad
Richard Lopes <richard.cirux@gmail.com>
2020-03-25 23:42:44 -0300

    refactor: remove duplicated folder

--- src/data/usecases/add-account/db-add-account.ts
+++ src/data/usecases/add-account/db-add-account.ts
@@ -1,9 +1,9 @@
-import { AccountModel } from '../../../../domain/models/account'
+import { AccountModel } from '../../../domain/models/account'
 import {
   AddAccount,
   AddAccountModel
-} from '../../../../domain/usecases/add-account'
-import { Encrypter } from '../../../protocols/encrypter'
+} from '../../../domain/usecases/add-account'
+import { Encrypter } from '../../protocols/encrypter'
 
 export class DbAddAccount implements AddAccount {
   private readonly encrypter: Encrypter;
